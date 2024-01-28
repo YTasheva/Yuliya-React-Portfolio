2f60571f91c75f4c2d0477d86fb10c538dea995e
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-28 11:28:50 +0000

    Refactor App component and add routing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import ProjectGallery from './pages/Project Gallery';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+    return (
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/home" element={<Home/>} />
+            <Route path="/project" element={<Project/>} />
+            <Route path="/project gallery" element={<Project Gallery/>} />
+            <Route path="/skills" element={<Skills/>} />
+            <Route path="/contact" element={<Contact/>} />
+          </Routes>
+        </Wrapper>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+    );
 }
 
 export default App
