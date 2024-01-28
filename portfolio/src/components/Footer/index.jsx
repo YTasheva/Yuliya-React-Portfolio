8bc5e786896bc3c68b33bf016776e46aa53a098f
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-28 11:30:02 +0000

    Add Footer component to portfolio

---
+++ portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+const HtmlToReactParser = require('html-to-react').Parser;
+const ReactDOMServer = require('react-dom/server');
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <span>Yuliya Tasheva 2024</span>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
