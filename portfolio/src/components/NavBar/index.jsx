c980407e08b4fb5ed6652c63825f74458735792f
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-28 10:53:58 +0000

    Add NavBar component to portfolio

---
+++ portfolio/src/components/NavBar/index.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './style.css';
+
+// Sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Project
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/discover"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Project Gallery
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/search"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Skills
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/search"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/search"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
