6a31ca6fadbee35f38084b80c36fe080f259afe0
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-29 22:53:48 +0000

    Refactor App component structure

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,33 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from "./components/Layout";
-import Home from './pages/Home';
-import Project from './pages/Project';
-import ProjectGallery from './pages/Project Gallery';
-import Skills from './pages/Skills';
-import Contact from './pages/Contact';
-import Footer from './components/Footer';
-import Wrapper from './components/Wrapper';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Navbar from './Navbar';
+import Navbar2 from './Navbar2';
+import Footer from './Footer';
 
-function App() {
+const Layout = () => {
     return (
-    <Router>
-      <div>
-        <Wrapper>
-          <Routes>
-            <Route path="/" element={<Layout/>} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/project" element={<Project/>} />
-            <Route path="/project gallery" element={<Project Gallery/>} />
-            <Route path="/skills" element={<Skills/>} />
-            <Route path="/contact" element={<Contact/>} />
-          </Routes>
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
-    );
+      <div className="App">
+      {<Header title="Yuliya's Portfolio" />}
+      {<Navbar  />}
+      <Navbar2  />
+      <Outlet />
+      <Footer />
+  </div>
+)
 }
 
-export default App
+export default Layout
