46b17cca28e9b3da762f58272ea5d858b623defe
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-29 22:53:01 +0000

    Add Layout component and pages

---
+++ portfolio/src/components/Layout/index.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from "./components/Layout";
+import About from './pages/About';
+import Discover from './pages/Discover';
+import Search from './pages/Search';
+import Navbar from './components/Navbar';
