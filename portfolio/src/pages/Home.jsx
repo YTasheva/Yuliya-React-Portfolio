8716b25555709dfbda9437f3ae773dfc83d60240
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 17:36:44 +0000

    Refactor layout and navbar components

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import Container from '../components/Container';
-import Row from '../components/Row';
-import Col from '../components/Col';
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import profileImage from "../images/womens face- profile.jpg";
 
 function Home() {
   return (
@@ -15,22 +16,33 @@ function Home() {
         <Row>
           <Col size="md-12">
             <h1>About Me</h1>
+            <hr />
+            <div className="container-clearfix">
+              <img className="w-100 mb-3" src={profileImage} alt="profile" />
+            </div>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
             <p>
-            I am a front-end developer student based in London who is currently transforming ideas into pixels with motive.
-            I love creating web-wide wonders, designs you will fall for and cherish. Where imagination meets creativity and creativity meets code. 
-            I design dreams, one click at a time, and create enjoyable digital interactions and experiences that click and stick!
-            I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+              I am a front-end developer student based in London who is
+              currently transforming ideas into pixels with motive. I love
+              creating web-wide wonders, designs you will fall for and cherish.
+              Where imagination meets creativity and creativity meets code. I
+              design dreams, one click at a time, and create enjoyable digital
+              interactions and experiences that click and stick! I'm quietly
+              confident, naturally curious, and perpetually working on improving
+              my chops one design problem at a time.
             </p>
             <br />
             <p>
-            I started my coding journey with the idea to make web designs that capture users and turn my imagination into
-            virtual reality. I've always been drawn to both the creative and analytical side of development.
-            I'm a creative problem solver with a passion for design and technology and always looking for opportunities.
-            I enjoy learning new things ad looking forward to work with others to create something amazing.
+              I started my coding journey with the idea to make web designs that
+              capture users and turn my imagination into virtual reality. I've
+              always been drawn to both the creative and analytical side of
+              development. I'm a creative problem solver with a passion for
+              design and technology and always looking for opportunities. I
+              enjoy learning new things ad looking forward to work with others
+              to create something amazing.
             </p>
           </Col>
         </Row>
@@ -40,5 +52,3 @@ function Home() {
 }
 
 export default Home;
-
-
