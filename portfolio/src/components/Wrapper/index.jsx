4a12c476c5e51b1d1616bc1f3dc4de02e86852f5
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 20:42:20 +0000

    Add PropTypes to Wrapper component

--- portfolio/src/components/Wrapper/index.jsx
+++ portfolio/src/components/Wrapper/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types"; 
 import "./style.css";
 
 function Wrapper(props) {
   return <main className="wrapper">{props.children}</main>;
 }
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Wrapper;
