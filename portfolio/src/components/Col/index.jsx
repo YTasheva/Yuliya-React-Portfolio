f42be8887cac688ff2cb8fc66451a82e269d7cba
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 17:37:11 +0000

    Refactor Col and Footer components

--- portfolio/src/components/Col/index.jsx
+++ portfolio/src/components/Col/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Col(props) {
-  const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+  const size = props.size
+    .split(" ")
+    .map((size) => "col-" + size)
+    .join(" ");
 
   return <div className={size}>{props.children}</div>;
 }
