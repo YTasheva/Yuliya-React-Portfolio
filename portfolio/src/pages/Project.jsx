4f38bd07e446dc2a3ad21ce01caf228a241ea660
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 13:34:40 +0000

    Fix variable name in Project component

---
+++ portfolio/src/pages/Project.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import projects from "../data.json";
+
+const Project = () => {
+    const { id } = useParams();
+    const project = projectGallery.find((project) => project.id === id);
+    
+    return (
+        <div className="container">
+        <div className="row">
+            <div className="col">
+            <h1>{project.id}</h1>
+            <h3>{project.description}</h3>
+            <div>
+            <a href={project.html_url} target="_blank" rel="noreferrer">
+                <button className="btn bg-dark text-white m-2"> 
+                <i className="fa-solid fa-github-link" aria-hidden="true"></i> GitHub
+                </button>
+            </a>
+            <a href={project.deployed_url} target="_blank" rel="noreferrer">
+                <button className="btn bg-dark text-white m-2">
+                <i className="fa-solid fa-external-link" aria-hidden="true"></i> Deployed
+                </button>
+            </a>
+            </div>
+            <img className="img-fluid" src={project.image_url} alt={projectGallery.id} />
+        </div>
+    </div>
+        <div className="row">
+            <div className="col">
+            <Link to="/projectGallery">← Back to Projects</Link>
+            </div>
+        </div>
+      </div>
+    );
+    };
+
+    export default Project;
