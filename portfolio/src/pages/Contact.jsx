88623427014a3cab32d208b98053522d35c6b888
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 17:36:51 +0000

    Refactor contact form in Contact component

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import Form from 'react-bootstrap/Form';
-      
+import Form from "react-bootstrap/Form";
+
 function Contact(props) {
   return (
     <div>
-        <h1 class name="text-center">Contact</h1>
-        <p>Feel free to get in touch with me via social media or email using the contact form below.</p>
-        <p>I would truly like to hear what you think about the content and website or share what is on your mind.</p>
-        <p>Thank you for visiting my website!</p>
+      <h1 class name="text-center">
+        Contact
+      </h1>
+      <p>
+        Feel free to get in touch with me via social media or email using the
+        contact form below.
+      </p>
+      <p>
+        I would truly like to hear what you think about the content and website
+        or share what is on your mind.
+      </p>
+      <p>Thank you for visiting my website!</p>
 
       <Form.Floating className="mb-3">
         <Form.Control
@@ -19,27 +27,26 @@ function Contact(props) {
         />
         <label htmlFor="floatingInputCustom">Name</label>
       </Form.Floating>
-        <Form.Floating>
+      <Form.Floating>
         <Form.Control
           id="floatingInputCustom"
           type="email"
           placeholder="name@example.com"
-          />
+        />
         <label htmlFor="floatingInputCustom">Email address</label>
       </Form.Floating>
       <FloatingLabel controlId="floatingTextarea" label="Message">
         <Form.Control
-           as="textarea"
-           placeholder="Leave a message here"
-           style={{ height: '100px' }}
-            />
+          as="textarea"
+          placeholder="Leave a message here"
+          style={{ height: "100px" }}
+        />
       </FloatingLabel>
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-      </div>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </div>
   );
 }
 
 export default Contact;
-
