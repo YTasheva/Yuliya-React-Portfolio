4e39942d6826bac3b752d99e1948ca8c6f5eede5
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 20:42:40 +0000

    Fix class names in NavBar component

--- portfolio/src/components/NavBar/index.jsx
+++ portfolio/src/components/NavBar/index.jsx
@@ -6,12 +6,12 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
+      <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           Home
         </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -19,7 +19,7 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
