97590e8fca0f742c917579864ee9bc76fb1c7c9e
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-30 20:42:04 +0000

    Update font awesome icons in Skills component

--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -10,36 +10,36 @@ function Skills() {
           <div className="list-unstyled row justify-content-evenly">
             <div className="col-4">
               <i
-                className="fa-brands fa-square-git fa-7x"
+                className="fab fa-git-square fa-7x"
                 aria-hidden="true"
               ></i>
               <p>Git</p>
             </div>
             <div className="col-4">
-              <i className="fa-brands fa-github fa-7x" aria-hidden="true"></i>
+              <i className="fab fa-github fa-7x" aria-hidden="true"></i>
               <p>GitHub</p>
             </div>
             <div className="col-4">
-              <i className="fa-brands fa-gitlab fa-7x" aria-hidden="true"></i>
+              <i className="fab fa-gitlab fa-7x" aria-hidden="true"></i>
               <p>GitLab</p>
             </div>
             <div className="col-4">
               <i
-                className="fa-brands fa-html5 fa-7x text-danger"
+                className="fab fa-html5 fa-7x text-danger"
                 aria-hidden="true"
               ></i>
               <p>HTML</p>
             </div>
             <div className="col-4">
               <i
-                className="fa-brands fa-css3-alt fa-7x text-primary"
+                className="fab fa-css3-alt fa-7x text-primary"
                 aria-hidden="true"
               ></i>
               <p>CSS</p>
             </div>
             <div className="col-4">
               <i
-                className="fa-brands fa-bootstrap fa-7x text-info"
+                className="fab fa-bootstrap fa-7x text-info"
                 aria-hidden="true"
               ></i>
               <p>Bootstrap</p>
@@ -47,21 +47,21 @@ function Skills() {
 
             <div className="col-4">
               <i
-                className="fa-brands fa-js fa-7x text-warning"
+                className="fab fa-js fa-7x text-warning"
                 aria-hidden="true"
               ></i>
               <p>Javascript</p>
             </div>
             <div className="col-4">
               <i
-                className="fa-brands fa-node fa-7x text-success"
+                className="fab fa-node-js fa-7x text-success"
                 aria-hidden="true"
               ></i>
               <p>Node.js</p>
             </div>
             <div className="col-4">
               <i
-                className="fa-brands fa-react fa-7x text-info"
+                className="fab fa-react fa-7x text-info"
                 aria-hidden="true"
               ></i>
               <p>React</p>
