4981decb92da48e011980cf2bce24e3cb8228f7c
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-31 03:32:41 +0000

    Refactor Container component

--- portfolio/src/components/Container/index.jsx
+++ portfolio/src/components/Container/index.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-const Container = (props) => {
-  return (
-    <div className="container" style={props.style}>
-      {props.children}
-    </div>
-  );
-};
+function Container(props) {
+  return <div className="container" style={props.style}>{props.children}</div>;
+}
 
 export default Container;
