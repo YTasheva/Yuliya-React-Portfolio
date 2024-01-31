b7465fd62209d22f1d89dd8d3f3de74765b8e409
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-31 03:32:15 +0000

    Remove PropTypes import from Wrapper component

--- portfolio/src/components/Wrapper/index.jsx
+++ portfolio/src/components/Wrapper/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types"; 
 import "./style.css";
 
 function Wrapper(props) {
   return <main className="wrapper">{props.children}</main>;
 }
-
-Wrapper.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Wrapper;
