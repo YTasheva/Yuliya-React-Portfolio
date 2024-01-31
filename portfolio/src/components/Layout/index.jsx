ca25d15f534c80d73641117eca53aa0c6085abc1
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-31 00:22:19 +0000

    Refactor Navbar components

--- portfolio/src/components/Layout/index.jsx
+++ portfolio/src/components/Layout/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
-import Navbar from "../Navbar";
-import Navbar2 from "../Navbar2";
+import NavBar from "../NavBar";
 import Footer from "../Footer";
 
 const Layout = () => {
   return (
     <div className="App">
       <Header title="Yuliya's Portfolio" />
-      <Navbar />
-      <Navbar2 />
+      <NavBar />
       <Outlet />
       <Footer />
     </div>
