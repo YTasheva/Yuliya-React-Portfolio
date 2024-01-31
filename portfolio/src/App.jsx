b6e7bb5aa54828d1406003e5be219dc009313ffe
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-31 03:32:56 +0000

    Fix import paths in App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import ProjectGallery from "./pages/ProjectGallery";
+import Project from './pages/Project';
 import Skills from './pages/Skills';
 import Contact from './pages/Contact';
-import NavBar from "./components/NavBar";
-import Project from './pages/Project'; 
+import NavBar from "./components/NavBar/index";
 import Footer from "./components/Footer/index";
-import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/index";
 import "./App.css";
 
 
@@ -20,8 +20,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/home" element={<Home/>} />
-            <Route path="/project" element={<Project/>} />
             <Route path="/projectGallery" element={<ProjectGallery/>} />
+            <Route path="/project" element={<Project/>} />
             <Route path="/skills" element={<Skills/>} />
             <Route path="/contact" element={<Contact/>} />
           </Routes>
