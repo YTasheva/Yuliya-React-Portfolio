0027714bb7fbf2cfaeb174248c9d20c71cb9691a
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-01-31 16:34:38 +0000

    Refactor file imports and fix formatting

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import ProjectGallery from "./pages/ProjectGallery";
-import Project from './pages/Project';
-import Skills from './pages/Skills';
-import Contact from './pages/Contact';
-import NavBar from "./components/NavBar/index";
-import Footer from "./components/Footer/index";
+import Project from "./pages/Project";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
 import Wrapper from "./components/Wrapper/index";
+import Footer from "./components/Footer/index";
 import "./App.css";
-
 
 function App() {
   return (
     <Router>
-    <div>
-      <NavBar />
+      <div className="App">
+        <NavBar />
         <Wrapper>
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/projectGallery" element={<ProjectGallery/>} />
-            <Route path="/project" element={<Project/>} />
-            <Route path="/skills" element={<Skills/>} />
-            <Route path="/contact" element={<Contact/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/projectgallery" element={<ProjectGallery />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Wrapper>
         <Footer />
       </div>
     </Router>
-    );
+  );
 }
 
-export default App
+export default App;
