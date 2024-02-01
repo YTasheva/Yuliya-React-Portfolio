b7ea13f486bed2850157a2521827be307b1bd7bf
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-01 18:23:57 +0000

    Fix formatting in main.jsx

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
+//import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
   <React.StrictMode>
-    <App />
+        <App />
   </React.StrictMode>
 );
