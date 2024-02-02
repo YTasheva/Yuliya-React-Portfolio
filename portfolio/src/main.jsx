0034a9196be3db04489065e55bcccdc12a7e7f96
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-02 16:45:32 +0000

    Refactor Contact page form layout

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
-//import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
