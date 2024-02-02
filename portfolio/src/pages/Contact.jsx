0294c7432becebdf171005edd6775f09f0ab028a
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-02 18:13:23 +0000

    Add react-router-dom and index.css imports to Contact.jsx

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import {
   CDBInput,
   CDBCard,
@@ -9,59 +10,60 @@ import {
   CDBAlert,
 } from "cdbreact";
 import Hero from "../components/Hero";
+import "../index.css";
 
-const Contact = ()  =>{
-  return ( 
-  <div>
-    <Hero backgroundImage="../images/ContactPage.jpg">
-      <h1>Contact</h1>
-    </Hero>
-    <CDBContainer>
-      <CDBCard style={{ width: "50rem" }}>
-        <CDBCardBody className="mx-4">
-        <div className="text-center mt-4 mb-2">
-            <p className="h4 font-weight-bold"> Get In Touch </p>
-          </div>
-          <CDBInput
-            label="Your name"
-            type="text"
-            icon="user"
-            iconClass="text-muted"
-          />
-          <CDBInput
-            label="Your email"
-            type="email"
-            icon="envelope"
-            iconClass="text-muted"
-          />
-          <CDBInput
-            label="Subject"
-            type="text"
-            icon="tags"
-            iconClass="text-muted"
-          />
-          <CDBInput
-            label="Your message"
-            type="textarea"
-            icon="pencil-alt"
-            iconClass="text-muted"
-          />
-          <CDBBtn
-            outline
-            color="danger"
-            style={{ width: "30%" }}
-            className="btn-block mt-5 mx-auto"
-          >
-            Send
-            <CDBIcon far icon="paper-plane" />
-          </CDBBtn>
-        </CDBCardBody>
-      </CDBCard>
-    </CDBContainer>
+const Contact = () => {
+  return (
+    <div>
+      <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/contactMe.png"></Hero>
+      
+      <CDBContainer>
+        <CDBCard style={{ width: "50rem" }}>
+          <CDBCardBody className="mx-4">
+            <div className="text-center mt-4 mb-2">
+              <p className="h4 font-weight-bold"> Get In Touch </p>
+            </div>
+            <CDBInput
+              label="Your name"
+              type="text"
+              icon="user"
+              iconClass="text-muted"
+            />
+            <CDBInput
+              label="Your email"
+              type="email"
+              icon="envelope"
+              iconClass="text-muted"
+            />
+            <CDBInput
+              label="Subject"
+              type="text"
+              icon="tags"
+              iconClass="text-muted"
+            />
+            <CDBInput
+              label="Your message"
+              type="textarea"
+              icon="pencil-alt"
+              iconClass="text-muted"
+            />
+            <CDBBtn
+              outline
+              color="danger"
+              style={{ width: "30%" }}
+              className="btn-block mt-5 mx-auto"
+            >
+              Send
+              <CDBIcon far icon="paper-plane" />
+            </CDBBtn>
+          </CDBCardBody>
+        </CDBCard>
+      </CDBContainer>
     </div>
-    );
+  );
 };
-  {/* <CDBAlert
+
+  /* <CDBAlert
   color="danger"
  style={{ width: "43rem" }}
 className={"mt-5 mx-5"}
@@ -69,7 +71,8 @@ dismiss
  >
   <strong>Oops!</strong> Validation errors occurred. Please confirm the
 fields and submit it again.
-</CDBAlert>  */}
+</CDBAlert>  
+} */
 
 
 export default Contact;
