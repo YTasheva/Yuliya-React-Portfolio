2c904ed51129483a58c36972aa0a045fe961156f
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 22:29:00 +0000

    Refactor project gallery component

--- portfolio/src/pages/ProjectGallery.jsx
+++ portfolio/src/pages/ProjectGallery.jsx
@@ -1,35 +1,32 @@
 import React from "react";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
+import projects from "../projects.json";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import "../index.css";
-import image1 from "../images/WaveShore.GIF";
-import image2 from "../images/Crow-woman 4.jpg";
-import image3 from "../images/Cat-with-the-cream 2.jpg";
-import image4 from "../images/zebraMood.jpg";
-import image5 from "../images/dolls-strings-art.jpg";
-import image6 from "../leopard-crow.jpg";
 
-function ProjectGallery() {
+// import { Link } from "react-router-dom";
+// import image1 from "../images/02-portfolio-1.jpg";
+// import image2 from "../images/Cat-with-the-cream 2.jpg";
+// import image3 from "../images/Crow-woman 4.jpg";
+// import image4 from "../images/dolls-strings-art.jpg";
+// import image5 from "../images/leopard-crow.jpg";
+// import image6 from "../images/zebraMood.jpg";
+
+function Projects() {
   return (
     <div>
-      <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/projects.jpg">
-    </Hero>
-      <main id="project" className="container-fluid">
-        <h1 className="text-center">Projects Gallery</h1>
-        <hr />
+      <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/projects.jpg"></Hero>
+      <hr />
+      <main id="projects" className="container-fluid">
+        <h1 className="text-center mb-4">Project Gallery</h1>
         <Container style={{ marginTop: 30 }}>
           <Row>
-            {project.map((project) => (
+            {projects.map((item) => (
               <Col size="md-4">
-                <Card
-                  key={project.id}
-                  project={project}
-                />
+                <Card key={item.id} item={item} />
               </Col>
             ))}
-
           </Row>
         </Container>
       </main>
@@ -37,4 +34,4 @@ function ProjectGallery() {
   );
 }
 
-export default ProjectGallery;
+export default Projects;
