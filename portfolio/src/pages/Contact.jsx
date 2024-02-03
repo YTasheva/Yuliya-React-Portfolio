0d632088d6734716478c7dc3e2aa45407077aac8
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 03:10:15 +0000

    Refactor Contact page component

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,78 +1,62 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {
-  CDBInput,
-  CDBCard,
-  CDBCardBody,
-  CDBIcon,
-  CDBBtn,
-  CDBContainer,
-  CDBAlert,
-} from "cdbreact";
 import Hero from "../components/Hero";
-import "../index.css";
+import { Container } from "react-bootstrap";
 
-const Contact = () => {
+function Contact(props) {
   return (
-    <div>
-      <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/contactMe.png"></Hero>
-      
-      <CDBContainer>
-        <CDBCard style={{ width: "50rem" }}>
-          <CDBCardBody className="mx-4">
-            <div className="text-center mt-4 mb-2">
-              <p className="h4 font-weight-bold"> Get In Touch </p>
-            </div>
-            <CDBInput
-              label="Your name"
-              type="text"
-              icon="user"
-              iconClass="text-muted"
-            />
-            <CDBInput
-              label="Your email"
-              type="email"
-              icon="envelope"
-              iconClass="text-muted"
-            />
-            <CDBInput
-              label="Subject"
-              type="text"
-              icon="tags"
-              iconClass="text-muted"
-            />
-            <CDBInput
-              label="Your message"
-              type="textarea"
-              icon="pencil-alt"
-              iconClass="text-muted"
-            />
-            <CDBBtn
-              outline
-              color="danger"
-              style={{ width: "30%" }}
-              className="btn-block mt-5 mx-auto"
-            >
-              Send
-              <CDBIcon far icon="paper-plane" />
-            </CDBBtn>
-          </CDBCardBody>
-        </CDBCard>
-      </CDBContainer>
-    </div>
+    <>
+<Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/contactMe.png" />
+<Container style={{ minHeight: '50%' }}>
+    <main id="contact">
+      <h1 className="text-center mt-3">Get In Touch</h1>
+      <form className="w-80 m-auto mt-5">
+        <div className="mb-5">
+          <label htmlFor="InputName" className="form-label">
+          <i className="bi bi-person-bounding-box m-2" aria-hidden="true"></i>
+            Your Name
+          </label>
+          <input
+            type="text"
+            className="form-control bg-light"
+            id="InputName"
+            aria-describedby="nameHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="InputEmail" className="form-label">
+          <i className="bi bi-envelope-at-fill m-2" aria-hidden="true"></i>
+            Your Email
+          </label>
+          <input
+            type="email"
+            className="form-control bg-light"
+            id="InputEmail"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text m-4">
+          I would truly like to hear what you think about the content and website or share what is on your mind.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="TextFormControl" className="form-label">
+          <i className="bi bi-chat-quote-fill m-2" aria-hidden="true"></i>
+            Your Message
+          </label>
+          <textarea
+            className="form-control bg-light"
+            id="TextFormControl"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-outline-danger mb-2">
+        <i className="bi bi-send m-1" aria-hidden="true"></i>
+          Send
+        </button>
+      </form>
+    </main>
+    </Container>
+    </>
   );
-};
-
-  /* <CDBAlert
-  color="danger"
- style={{ width: "43rem" }}
-className={"mt-5 mx-5"}
-dismiss
- >
-  <strong>Oops!</strong> Validation errors occurred. Please confirm the
-fields and submit it again.
-</CDBAlert>  
-} */
-
+}
 
 export default Contact;
