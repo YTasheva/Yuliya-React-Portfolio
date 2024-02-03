acc6e3208291a0766cfa3f003b2f3d5ed434ed0d
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 03:10:29 +0000

    Refactor Card component to use Link and display project details

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { CDBCard } from "cdbreact";
+import { Link } from "react-router-dom";
+import "../index.css";
 
-const Card = (props) => {
+const Card = ({ project }) => {
   return (
-    <div
-      className="card "
-      style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none",
-      }}
-    >
-      {!props.image && (
-        <i className="fa fa-spinner fa-spin" aria-hidden="true" />
-      )}
+    <div className="card">
+      <Link to={`{project.id}`}>
+        <h3 className="card-title">{item.id}</h3>
+      </Link>
+      <p className="card-text">{item.description}</p>
+      <Link to={`{project.id}`}>
+        <img src={project.image_url} className="img-fluid img-thumbnail" alt={project.id} />
+      </Link>
     </div>
   );
-}
+};
 
 export default Card;
 
