03abe09594081ceab18d6d83542f8ecf2b7ba8bf
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 22:29:29 +0000

    Update Card and NavBar components

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
 
-const Card = ({ project }) => {
+const Card = ({ item }) => {
   return (
     <div className="card">
-      <Link to={`{project.id}`}>
+      <Link to={`{item.id}`}>
         <h3 className="card-title">{item.id}</h3>
       </Link>
       <p className="card-text">{item.description}</p>
-      <Link to={`{project.id}`}>
-        <img src={project.image_url} className="img-fluid img-thumbnail" alt={project.id} />
+      <Link to={`{item.id}`}>
+        <img src={item.image_url} className="img-fluid img-thumbnail" alt={item.id} />
       </Link>
     </div>
   );
