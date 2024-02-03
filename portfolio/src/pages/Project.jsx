f43c2f2e27f7cdd044db67e57acc6c4a5e9e3463
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 22:29:09 +0000

    Refactor Project component to use react-bootstrap

--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -1,44 +1,62 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import projects from "../projects.json";
 
 const Project = () => {
   const { id } = useParams();
-  const project = projects.find((project) => project.id === id);
-
+  const project = projects.find((item) => item.id === id);
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1>{project.id}</h1>
-          <h3>{project.description}</h3>
-          <div>
-            <a href={project.html_url} target="_blank" rel="noreferrer">
-              <button className="btn bg-dark text-white m-2">
-                <i className="fa-solid fa-github-link" aria-hidden="true"></i>{" "}
-                GitHub
-              </button>
-            </a>
-            <a href={project.deployed_url} target="_blank" rel="noreferrer">
-              <button className="btn bg-dark text-white m-2">
-                <i className="fa-solid fa-external-link" aria-hidden="true"></i>{" "}
-                Deployed
-              </button>
-            </a>
-          </div>
-          <img
-            className="img-fluid"
-            src={project.image_url}
-            alt={projects.id}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <Link to="/projects">← Back to Projects</Link>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <main id="project">
+            <article className="text-center">
+              {project && (
+                <div>
+                  <h1>{project.id}</h1>
+                  <p>{project.description}</p>
+                  <div>
+                    <a href={project.html_url} target="_blank" rel="noreferrer">
+                      <button className="btn bg-dark text-white m-2">
+                        <i
+                          className="fa-solid fa-github-link"
+                          aria-hidden="true"
+                        ></i>{" "}
+                        GitHub
+                      </button>
+                    </a>
+                    <a
+                      href={project.deployed_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button className="btn bg-dark text-white m-2">
+                        <i
+                          className="fa-solid fa-external-link"
+                          aria-hidden="true"
+                        ></i>{" "}
+                        Deployed
+                      </button>
+                    </a>
+                  </div>
+                  <img
+                    className="img-fluid"
+                    src={project.image_url}
+                    alt={projects.id}
+                  />
+                </div>
+              )}
+              {!project && (
+                <div>
+                  <Link to="/projects">← Back to Projects</Link>
+                </div>
+              )}
+            </article>
+          </main>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
