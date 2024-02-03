50b01f2f04c36921762d5c854870133afcd489dd
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 22:29:04 +0000

    Update Skills page with font awesome icons

--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -2,49 +2,47 @@ import React from "react";
 import Hero from "../components/Hero";
 import "../index.css";
 
-
 function Skills() {
   return (
     <>
-    <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/Skills.jpg">
-      </Hero>
+      <Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/Skills.jpg" />
       <main id="skills">
-      <hr />
+        <hr />
         <h1 className="text-center mb-4">Skills</h1>
         <div className="container text-center">
           <div className="list-unstyled row justify-content-evenly">
             <div className="col-4">
               <i
-                className="fab fa-git-square fa-7x"
+                className="fa-brands fa-git-square fa-7x"
                 aria-hidden="true"
               ></i>
               <p>Git</p>
             </div>
             <div className="col-4">
-              <i className="fab fa-github fa-7x" aria-hidden="true"></i>
+              <i className="fa-brands fa-github fa-7x" aria-hidden="true"></i>
               <p>GitHub</p>
             </div>
             <div className="col-4">
-              <i className="fab fa-gitlab fa-7x" aria-hidden="true"></i>
+              <i className="fa-brands fa-gitlab fa-7x" aria-hidden="true"></i>
               <p>GitLab</p>
             </div>
             <div className="col-4">
               <i
-                className="fab fa-html5 fa-7x text-danger"
+                className="fa-brands fa-html5 fa-7x text-danger"
                 aria-hidden="true"
               ></i>
               <p>HTML</p>
             </div>
             <div className="col-4">
               <i
-                className="fab fa-css3-alt fa-7x text-primary"
+                className="fa-brands fa-css3-alt fa-7x text-primary"
                 aria-hidden="true"
               ></i>
               <p>CSS</p>
             </div>
             <div className="col-4">
               <i
-                className="fab fa-bootstrap fa-7x text-info"
+                className="fa-brands fa-bootstrap fa-7x text-info"
                 aria-hidden="true"
               ></i>
               <p>Bootstrap</p>
@@ -52,21 +50,21 @@ function Skills() {
 
             <div className="col-4">
               <i
-                className="fab fa-js fa-7x text-warning"
+                className="fa-brands fa-js fa-7x text-warning"
                 aria-hidden="true"
               ></i>
               <p>Javascript</p>
             </div>
             <div className="col-4">
               <i
-                className="fab fa-node-js fa-7x text-success"
+                className="fa-brands fa-node-js fa-7x text-success"
                 aria-hidden="true"
               ></i>
               <p>Node.js</p>
             </div>
             <div className="col-4">
               <i
-                className="fab fa-react fa-7x text-info"
+                className="fa-brands fa-react fa-7x text-info"
                 aria-hidden="true"
               ></i>
               <p>React</p>
