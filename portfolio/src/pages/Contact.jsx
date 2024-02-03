c3fbdc5ebbe650f51ac9ce78ba143d76395bb029
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-03 17:02:15 +0000

    Add form functionality to Contact page

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,60 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "../components/Hero";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Contact(props) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const initialState = { name: "", email: "", message: "" };
+  // const [eachEntry, setEachEntry] = useState(initialState);
+  // const { name, email, message } = eachEntry;
+
+  const handleInputChange = (e) => {
+    setEachEntry({ ...eachEntry, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`name is ${formData.name}`);
+    console.log(`email is ${formData.email}`);
+    console.log(`message is ${formData.message}`);
+
+    props.doSomething(formData);
+    setFormData(initialState);
+  };
+
   return (
     <>
-<Hero backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/contactMe.png" />
-<Container style={{ minHeight: '50%' }}>
-    <main id="contact">
-      <h1 className="text-center mt-3">Get In Touch</h1>
-      <form className="w-80 m-auto mt-5">
-        <div className="mb-5">
-          <label htmlFor="InputName" className="form-label">
-          <i className="bi bi-person-bounding-box m-2" aria-hidden="true"></i>
-            Your Name
-          </label>
-          <input
-            type="text"
-            className="form-control bg-light"
-            id="InputName"
-            aria-describedby="nameHelp"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="InputEmail" className="form-label">
-          <i className="bi bi-envelope-at-fill m-2" aria-hidden="true"></i>
-            Your Email
-          </label>
-          <input
-            type="email"
-            className="form-control bg-light"
-            id="InputEmail"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text m-4">
-          I would truly like to hear what you think about the content and website or share what is on your mind.
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="TextFormControl" className="form-label">
-          <i className="bi bi-chat-quote-fill m-2" aria-hidden="true"></i>
-            Your Message
-          </label>
-          <textarea
-            className="form-control bg-light"
-            id="TextFormControl"
-            rows="3"
-          ></textarea>
-        </div>
-        <button type="submit" className="btn btn-outline-danger mb-2">
-        <i className="bi bi-send m-1" aria-hidden="true"></i>
-          Send
-        </button>
+      <Hero
+        className="w-10"
+        backgroundImage="https://raw.githubusercontent.com/YTasheva/Yuliya-React-Portfolio/main/portfolio/src/images/contactMe.png"
+      />
+
+      <div className="text-center mt-3 h4 font-weight-bold">
+        <h2>Get In Touch</h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <form onSubmit={handleInputChange}>
+          <Container className="w-80 m-auto mt-5">
+            <Row className="mb-5">
+              <Col size="12">
+                <label htmlFor="Name" className="form-label">
+                  <i
+                    className="bi bi-person-bounding-box m-2"
+                    aria-hidden="true"
+                  ></i>
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control bg-light"
+                  id="Name"
+                  aria-describedby="nameHelp"
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col size="12">
+                <label htmlFor="Email" className="form-label">
+                  <i
+                    className="bi bi-envelope-at-fill m-2"
+                    aria-hidden="true"
+                  ></i>
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control bg-light"
+                  id="Email"
+                  aria-describedby="emailHelp"
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+                <div id="emailHelp" className="form-text m-4">
+                  I would truly like to hear what you think about the content
+                  and website or share what is on your mind.
+                </div>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col size="12">
+                <label htmlFor="Message" className="form-label">
+                  <i
+                    className="bi bi-chat-quote-fill m-2"
+                    aria-hidden="true"
+                  ></i>
+                  Your Message
+                </label>
+                <textarea
+                  className="form-control bg-light"
+                  id="Message"
+                  rows="3"
+                  onChange={(e) =>
+                    setFormData({ ...formData, message: e.target.value })
+                  }
+                ></textarea>
+              </Col>
+            </Row>
+            <button type="submit" className="btn btn-outline-danger mb-2">
+              <i className="bi bi-send m-1" aria-hidden="true"></i>
+              Send
+            </button>
+          </Container>
+          <Container className="mt-4">
+            <h3>Awesome {formData.username}!</h3>
+            <p>
+              I should tell you this, your interest in my site is much
+              appreciated {formData.password}!
+            </p>
+          </Container>
+        </form>
       </form>
-    </main>
-    </Container>
     </>
   );
 }
