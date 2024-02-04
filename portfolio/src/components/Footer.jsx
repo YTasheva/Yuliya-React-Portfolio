f7e16802ff5c5756dc9972d2af3566ec666ab5ba
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-04 03:59:35 +0000

    Update Footer component and index.css

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-
 const Footer = () => {
   const today = new Date();
   return (
     <footer className="footer text-center bg-light text-dark container-fluid flex-column pt-3">
       <div className="container-fluid p-3 gap-2 d-grid d-md-flex flex-row flex-wrap justify-content-around">
         <a href="tel:+5555555555" className="btn btn-light">
-          <i className="fa fa-phone-square" aria-hidden="true"></i>
+          <i className="fa fa-phone-square m-1" aria-hidden="true"></i>
           555.555.5555
         </a>
         <a href="mailto:ytasheva@website.com" className="btn btn-light">
-          <i className="fa fa-envelope-square" aria-hidden="true"></i>
+          <i className="fa fa-envelope-square m-1" aria-hidden="true"></i>
           ytashevagit@gmail.com
         </a>
         <a
@@ -36,14 +35,17 @@ const Footer = () => {
           rel="noopener noreferrer"
           className="btn-light"
         >
-          <i className="bi bi-filetype-pdf m-2" aria-hidden="true"></i> CV / Resume{" "}
+          <i className="bi bi-filetype-pdf m-1" aria-hidden="true"></i> CV /
+          Resume{" "}
         </a>
       </div>
       <div className="text-center d-block bg-light text-dark container-fluid pt-3">
-        <ul>Yuliya Tasheva &copy; {today.getFullYear()}</ul>
+        <ul>
+          <b>Yuliya Tasheva &copy; {today.getFullYear()}</b>
+        </ul>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
