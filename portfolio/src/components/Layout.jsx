184dddb39452fefc450ace93b97aab80b5b1e33d
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-05 14:15:39 +0000

    Remove Header component and add Projects page

--- portfolio/src/components/Layout.jsx
+++ portfolio/src/components/Layout.jsx
@@ -1,18 +1,15 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <div className="App">
-      <Header title="Yuliya's Portfolio" />
       <NavBar />
       <Outlet />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
 export default Layout;
