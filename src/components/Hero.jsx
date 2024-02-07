1bad15e7436bc882c3cede6ee388321b300437dd
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-07 22:31:44 +0000

    Add new images and components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero(props) {
+  return (
+    <div
+      className="hero text-center"
+      style={{ backgroundImage: `url(${props.backgroundImage})`,
+    position: 'relative'
+   }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+
+export default Hero;
