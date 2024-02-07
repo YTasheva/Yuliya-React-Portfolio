8a73b15dc5753abcc8cb3eeab2f82a0db968ea9c
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-07 15:36:34 +0000

    Fix Link to item.id in Card component

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -5,11 +5,11 @@ import "../index.css";
 const Card = ({ item }) => {
   return (
     <div className="card mb-5">
-      <Link to={`{item.id}`}>
+      <Link to={item.id}>
         <h3 className="card-title">{item.id}</h3>
       </Link>
       <p className="card-text">{item.description}</p>
-      <Link to={`{item.id}`}>
+      <Link to={item.id}>
         <img
           src={item.image_url}
           className="img-fluid img-thumbnail"
