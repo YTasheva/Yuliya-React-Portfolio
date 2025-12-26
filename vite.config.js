e79b7fd2ec700262decc9689d6cc68c81c57e1ac
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2025-12-26 18:20:08 +0000

    add vite production deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const base =
-  process.env.NODE_ENV === "production"
-    ? "/Yuliya-React-Portfolio/"
-    : "/";
+const base = process.env.VITE_BASE_PATH || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
